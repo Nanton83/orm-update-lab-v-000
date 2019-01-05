59a67f35f743f9edd647f4d9dac1f9b7e289ef5b
Nick Romans <nicholas.romans@gmail.com>
2019-01-05 18:06:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,12 @@ class Student
   end
 
   def save
+    sql = <<-SQL
+    INSERT INTO students(name, grade)
+    VALUES(?,?)
+    SQL
+
+    DB[:conn].execute(sql, name, grade)
   end
 
   def self.create
